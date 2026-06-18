78ca14603d39e9dacb4dad654cba049f0c4706e5
viedietvijay-lab <viedietvijay@gmail.com>
2026-06-18 14:50:19 +0530

    Update upi.py

--- upi.py
+++ upi.py
@@ -7,7 +7,7 @@ from database import Database
 from points_manager import PointsManager
 from buttons import back_button, main_menu
 
-bot = telebot.TeleBot(BOT_TOKEN)
+from bot_instance import bot
 db = Database()
 pm = PointsManager()
 
